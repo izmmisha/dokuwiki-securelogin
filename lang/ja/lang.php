a0f988df6e1b415981c6cebd926f86af5c9e02ef
Hideaki SAWADA <chuno@live.jp>
2014-09-23 07:58:40 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,22 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['generate_key'] = '新しい鍵ペアを生成します';
+$lang['generate'] = '新規キーの生成';
+$lang['set'] = '設定';
+$lang['key_length'] = '鍵長';
+$lang['public_key'] = '公開鍵';
+$lang['key_modulus'] = '鍵 modulus';
+$lang['key_exponent'] = '公開鍵 exponent';
+$lang['test_key'] = 'テスト鍵';
+$lang['test_message'] = 'テストメッセージ';
+$lang['test'] = 'Test';
+$lang['use_securelogin'] = '安全なログイン';
+$lang['use_secureupdate'] = 'パスワードの安全な転送を使用';
+$lang['securelogin_conf'] = '安全なログイン設定S';
+$lang['sample_message'] = 'これはテストメッセージです。';
